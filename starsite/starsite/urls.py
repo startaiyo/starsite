d24d4fde048b1afb8bbfa74ab037ac69ed8c8a6e
startaiyo <startaiyo0104@gmail.com>
2021-03-22 23:04:35 +0900

    secure

--- starsite/starsite/urls.py
+++ starsite/starsite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('starapp/', include('star_app.urls') ),
+    path('', include('star_app.urls') ),
 ]
