5de76700a150d45cff5502140df97eac88744d59
startaiyo <startaiyo0104@gmail.com>
2021-03-25 22:15:18 +0900

    form applied

--- starsite/star_app/models.py
+++ starsite/star_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Work(models.Model):
+    content=models.TextField()
+    day=models.IntegerField()
+    date=models.DateTimeField()
+    interval=models.IntegerField()
--- starsite/star_app/views.py
+++ starsite/star_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from . import forms
+from star_app.models import Work
 
 # Create your views here.
 def index(request):
@@ -9,4 +10,14 @@ def alert(request):
     return render(request,'star_app/alert.html')
 
 def register(request):
-    return render(request,'star_app/index.html')
+    form = forms.WorkModelForm()
+    if request.method=='POST':
+        form=forms.WorkModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,'star_app/index.html',context={'form':form})
+
+def all_delete(request):
+    work=Work.objects.all()
+    work.delete()
+    return render(request,'star_app/deleted.html')
