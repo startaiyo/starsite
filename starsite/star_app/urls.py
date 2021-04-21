3a20e1f677d2915c8d168fe97190dfa98d447bed
startaiyo <startaiyo0104@gmail.com>
2021-04-21 23:16:39 +0900

    lunchmap

--- starsite/star_app/urls.py
+++ starsite/star_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('all_delete/',views.all_delete,name='all_delete'),
     path('create/', views.UserCreateView.as_view(),name="create"),
     path('delete/<int:id>/', views.delete,name='delete'),
+    path('lunchmap/', views.lunchmap,name='lunchplace'),
 ]
